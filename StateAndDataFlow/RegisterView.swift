d961f14807fa361d8a0e9d042a51c572ab0dfa29
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-10-21 14:28:04 +0300

    Added disable login functionality

--- StateAndDataFlow/RegisterView.swift
+++ StateAndDataFlow/RegisterView.swift
@@ -11,6 +11,7 @@ struct RegisterView: View {
     @State private var name = ""
     @EnvironmentObject private var user: UserManager
     @ObservedObject var textCounter = TextCounter()
+    @State var textColor = Color(.red)
     
     var body: some View {
         VStack {
@@ -18,6 +19,7 @@ struct RegisterView: View {
                 TextField("Enter your name", text: $textCounter.text)
                     .multilineTextAlignment(.center)
                 Text("\(textCounter.counter)")
+                    .foregroundColor(setColor())
                 Spacer()
             }
             Button(action: registerUser) {
@@ -25,15 +27,28 @@ struct RegisterView: View {
                     Image(systemName: "checkmark.circle")
                     Text("OK")
                 }
+                .disabled(textCounter.text.count < 3)
             }
         }
     }
     
     private func registerUser() {
+
         if !name.isEmpty {
             user.name = name
             user.isRegister.toggle()
         }
+    }
+    
+    private func setColor() -> Color {
+        if let counter = Int(textCounter.counter) {
+            if counter >= 3 {
+                textColor = Color(.gray)
+            } else {
+                textColor = Color(.gray)
+            }
+        }
+        return textColor
     }
 }
 
